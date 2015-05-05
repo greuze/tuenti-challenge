41bca3fb6ace5db450d81753151ea5e7ed77008b
Iván López <ils@tid.es>
2015-05-05 19:19:23 +0200

    Much faster

--- 2015/6/airscrews.js
+++ 2015/6/airscrews.js
@@ -1,9 +1,48 @@
 var fs = require('fs');
 
 var DATA = [];
+var PARTS = {};
 
 var toInt = function(e) {
     return parseInt(e);
+};
+
+var calculatePart = function(x, y, size) {
+    var sum = 0;
+    var part = PARTS['r-' + x + '-' + y + '-' + size];
+    if (part !== undefined) {
+        sum = part;
+    } else {
+        part = PARTS['r-' + (x - 1) + '-' + y + '-' + size];
+//        if (false) {
+        if (part !== undefined) {
+            x1 = x - 1;
+            x2 = x1 + size;
+//            console.log('para r-' + x + '-' + y + '-' + size + ' reciclo r-' + x1 + '-' + y + '-' + size + '=' + part);
+            sum = part;
+            for (var k = y; k < y + size; k++) {
+//                console.log('resto %d', DATA[k][x1]);
+                sum -= DATA[k][x1];
+//                console.log('sumo %d', DATA[k][x2]);
+                sum += DATA[k][x2];
+            }
+            PARTS['r-' + x + '-' + y + '-' + size] = sum;
+//            console.log('reciclado r-' + x + '-' + y + '-' + size + '=' + sum);
+        } else {
+            for (var k = y; k < y + size; k++) {
+                for (var l = x; l < x + size; l++) {
+                    sum += DATA[k][l];
+                }
+            }
+            PARTS['r-' + x + '-' + y + '-' + size] = sum;
+//            console.log('r-' + x + '-' + y + '-' + size + '=' + sum);
+        }
+    }
+    return sum;
+};
+
+var calculateValue = function(x, y, size) {
+    return calculatePart(x, y, size) + calculatePart(x + size + 1, y + size + 1, size);
 };
 
 var printAirscrew = function(index, myCase) {
@@ -15,15 +54,9 @@ var printAirscrew = function(index, myCase) {
     var totalSize = 2 * partSize + 1;
 
     var max = 0;
-    for (var i = y0; i <= y1 - totalSize + 1; i++) {
-        for (var j = x0; j <= x1 - totalSize + 1; j++) {
-            var sum = 0;
-            for (var k = i; k < i + partSize; k++) {
-                for (var l = j; l < j + partSize; l++) {
-                    sum += DATA[k][l]; // first part
-                    sum += DATA[k + partSize + 1][l + partSize + 1]; // second part
-                }
-            }
+    for (var i = x0; i <= x1 - totalSize + 1; i++) {
+        for (var j = y0; j <= y1 - totalSize + 1; j++) {
+            var sum = calculateValue(i, j, partSize);
             if (sum > max) {
                 max = sum;
             }
